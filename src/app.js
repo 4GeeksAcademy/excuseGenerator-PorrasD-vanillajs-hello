16dcfdbd74d0840bbdd082e148850fdfbf5a5f4a
PorrasD <122325175+PorrasD@users.noreply.github.com>
2024-11-14 03:51:43 +0000

    excuse generator completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function excuses() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let person = Math.floor(Math.random() * who.length);
+  let verb = Math.floor(Math.random() * action.length);
+  let adjective = Math.floor(Math.random() * what.length);
+  let time = Math.floor(Math.random() * when.length);
+  return (
+    who[person] + " " + action[verb] + " " + what[adjective] + " " + when[time]
+  );
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuses();
+    document.querySelector("#excuse").innerHTML = excuses();
+  });
 };
